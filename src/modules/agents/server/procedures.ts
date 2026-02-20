4796ec55adb34f5824d026c50896585af47944ce
YauCode <yauheni.baikou88@gmail.com>
2026-02-20 17:52:35 +0200

    14: agent page

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,3 +1,4 @@
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
 
@@ -10,9 +11,10 @@ import { agentsInsertSchema } from "../schemas";
 
 
 
+
 export const agentsRouter = createTRPCRouter({
 
-    getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
+    getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input, ctx }) => {
         const [existingAgent] = await db
             .select({
                 // TODO: Change to actual count
@@ -20,7 +22,16 @@ export const agentsRouter = createTRPCRouter({
                 ...getTableColumns(agents),
             })
             .from(agents)
-            .where(eq(agents.id, input.id))
+            .where(
+                and(
+                    eq(agents.id, input.id),
+                    eq(agents.userId, ctx.auth.user.id),
+                )
+            );
+
+        if (!existingAgent) {
+            throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found" });
+        }
         return existingAgent;
     }),
 
